a0d37a3b2ceece263c2d25356ec80dc579869ecc
tdavis36 <196705197+tdavis36@users.noreply.github.com>
2025-12-01 19:55:33 -0500

    fix: adding updated main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,8 +4,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './assets/styles/variables.css'
+import { createInternalToastAPI } from './composables/use-toast.ts'
 
 const app = createApp(App)
+
+const toastAPI = createInternalToastAPI()
+app.provide('toast', toastAPI)
 
 app.use(createPinia())
 app.use(router)
