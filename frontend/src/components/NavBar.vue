c4d25bf8f49edd2ff4d96655335db8ac4ffbfe61
Caroline Young <Cyoung29@ycp.edu>
2026-02-18 12:49:37 -0500

    Fully working admin dashboard and admin button

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -22,10 +22,10 @@ const emit = defineEmits(["logout"])
 
         <template v-if="loggedIn">
 
-        <template v-if="isAdmin">
+        <template v-if="isAdmin && isInstructor">
           <div class="nav-divider"></div>
           <router-link to="/admin-dashboard" class="nav_button">Admin</router-link>
-          </template>
+        </template>
 
           <div class="nav-divider"></div>
           <router-link to="/settings" class="nav_button">Settings</router-link>
