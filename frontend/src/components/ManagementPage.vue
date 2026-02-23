a09186f9ea3ab27171ff85e1c676a49c65516775
Caleb Jones (Personal) <cjones76@ycp.edu>
2026-02-22 20:08:52 -0500

    Fixed SectionViewPage import error

--- frontend/src/components/ManagementPage.vue
+++ frontend/src/components/ManagementPage.vue
@@ -64,7 +64,7 @@
         </div>
 
         <div v-else-if="selectedCourseId" class="view-container">
-          <SemesterViewPage :course-id="selectedCourseId" />
+          <SectionViewPage :course-id="selectedCourseId" />
         </div>
 
         <div v-else class="empty-state">
@@ -92,7 +92,7 @@
 <script setup lang="ts">
 import { ref, onMounted, watch } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
-import SemesterViewPage from '@/components/pages/SemesterViewPage.vue'
+import SectionViewPage from '@/components/pages/SectionViewPage.vue'
 import InstructorSelector from '@/components/pages/InstructorSelector.vue'
 import api from '@/api'
 import CourseEditorModal from "@/components/CourseEditorModal.vue";
