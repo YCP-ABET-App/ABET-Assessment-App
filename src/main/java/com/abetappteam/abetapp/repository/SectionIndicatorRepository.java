90ef411609925b272f96ddaf001d8f2cee7396c9
Emmet Larson <elars1002@gmail.com>
2026-03-16 23:38:03 -0400

    Implement SectionIndicator search method

--- src/main/java/com/abetappteam/abetapp/repository/SectionIndicatorRepository.java
+++ src/main/java/com/abetappteam/abetapp/repository/SectionIndicatorRepository.java
@@ -10,7 +10,9 @@ import java.util.List;
 public interface SectionIndicatorRepository extends JpaRepository<SectionIndicator, Long> {
 
     @Query("SELECT DISTINCT si FROM SectionIndicator si " +
-            "")
+            "WHERE (:ids IS NULL OR si.id IN :ids) " +
+            "AND (:sectionIds IS NULL OR si.sectionId IN :sectionIds) " +
+            "AND (:indicatorIds IS NULL OR si.indicatorId IN :indicatorIds)")
     List<SectionIndicator> searchSectionIndicators(
             @Param("ids") List<Integer> ids,
             @Param("sectionIds") List<Integer> sectionIds,
